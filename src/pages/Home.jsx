d7c98472d54d9641347e9e9aa10cc827459725b7
cristianmoletto <cristian.moletto@gmail.com>
2025-11-22 11:55:55 +0100

    feat: routing pagine base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,32 @@
-export default function Home() {
- return <h1>Home Page</h1>;
+import { Link, NavLink } from "react-router-dom";
 
+export default function Home() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-sm">
+        <NavLink className="navbar-brand" to="">
+          Navbar
+        </NavLink>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/chi-siamo">
+                Chi Siamo
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/prodotti">
+                Prodotti
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
